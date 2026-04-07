66dd49f7c3c4439cad3b367f211ab85c7a3da969
darzafrui <darzafrui@alum.us.es>
2026-04-07 22:48:47 +0200

    fix: image delete

--- frontend/src/features/home/ui/components/QuestionListItem.js
+++ frontend/src/features/home/ui/components/QuestionListItem.js
@@ -4,7 +4,6 @@ import {
     Text,
     StyleSheet,
     TouchableOpacity,
-    Image,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { CountdownText } from './CountdownText';
