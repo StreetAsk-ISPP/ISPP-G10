c8ca285bfbd72624610c471cfbd5be603d473b7c
josegicar <josegicar@alum.us.es>
2026-05-05 16:19:09 +0200

    fix: delete setAccesible

--- src/test/java/com/streetask/app/util/RestPreconditionsTest.java
+++ src/test/java/com/streetask/app/util/RestPreconditionsTest.java
@@ -2,8 +2,6 @@ package com.streetask.app.util; // Asegúrate de que este es el paquete correcto
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
@@ -11,14 +9,8 @@ import org.junit.jupiter.api.Test;
 class RestPreconditionsTest {
 
     @Test
-    @SuppressWarnings({ "reflection", "removal" })
     void privateConstructorShouldThrowAssertionError() throws NoSuchMethodException {
         Constructor<RestPreconditions> constructor = RestPreconditions.class.getDeclaredConstructor();
-
-        AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
-            constructor.setAccessible(true);
-            return null;
-        });
 
         assertThatThrownBy(() -> constructor.newInstance())
                 .isInstanceOf(InvocationTargetException.class)
